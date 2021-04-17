b03a382aff34333535a288ea3fc966ff3d551b2f
adarsh01-debug <adarsh.official2299@gmail.com>
2021-04-17 21:31:22 +0530

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import {DiCoffeescript, DiReact} from 'react-icons/di';
+import {FaHeart} from 'react-icons/fa';
 
 function About() {
     return (
-        <div to="/about">
-            About
+        <div to="/about" className="about">
+            <h1>About</h1>
+            <p>Find about all your favorite movies at one place. Search your movie with name (and year) and persuade the inner movie-holic inside you.</p>
+            <h4>Made with <DiCoffeescript size={20}/>, <DiReact size={20}/> and <FaHeart/></h4>
+            <h5>Copyright © - adarsh01-debug</h5>
         </div>
     )
 }
